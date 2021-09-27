b32e31af8c28ac7266789751547c339eba2d2425
Keegan Evans <keegan.evans@gmail.com>
2021-09-27 11:36:11 -0700

    PROG: finished fib exercise, ch3

--- rust-lang-class/fib/src/main.rs
+++ rust-lang-class/fib/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    fib(3);
+    let n_th_fib = fib(35, 0, 1);
+    println!("{}", n_th_fib);
 }
 
-fn fib(n: isize) {
-    if n > 0 {
-        println!("{}", n);
-        fib(n - 1);
+fn fib(n: u32, n_2: u32, n_1: u32) -> u32 {
+    if n == 3 {
+        n_2 + n_1
+    } else {
+        fib(n - 1, n_1, n_2 + n_1)
     }
 }
