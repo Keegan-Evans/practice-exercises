d6ae6ffdebbae70563844a0aeb5c54b9cae4f7e9
Keegan Evans <keegan.evans@gmail.com>
2021-09-29 19:59:22 -0700

    PROG: ch4 complete

--- rust-lang-class/slicey/src/main.rs
+++ rust-lang-class/slicey/src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused)]
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello, world!";
+    
+    //let mut s = String::from("hello world");
+    //println!("{}", &s[..5]);
+
+    //let word = first_word(&s);
+
+    //s.clear();
+
+    //println!("The index of the end of the first word is: {}", word);
 }
